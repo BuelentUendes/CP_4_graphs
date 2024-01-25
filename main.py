9eea0a93c8fa0d420cc6da9e75f8ee968ddb2dfb
BuelentUendes <buelent.tt@web.de>
2024-01-25 09:59:07 +0100

    Adjusted the main script to incorporate the changes resulting from the different config files

--- main.py
+++ main.py
@@ -40,7 +40,8 @@ def main(args):
     #Set the seed for reproducibility
     seed_everything(args.random_seed)
 
-    model_config = load_yaml_config_file(os.path.join(CONFIG_PATH, args.model_config_file))
+    model_config = load_yaml_config_file(os.path.join(CONFIG_PATH, args.models_config_file))
+    training_config = load_yaml_config_file(os.path.join(CONFIG_PATH, args.training_config_file))
     for dataset_name in args.dataset:
         #Retrieve the data
         dataset = get_data(dataset_name, root=DATA_PATH, homophily=args.homophily)
@@ -56,38 +57,41 @@ def main(args):
                                                                                                   percentage_tuning_data=0.5)
         #Train each model
         for model_type in args.model:
-            model = get_model(model_type, num_features, num_classes, **model_config["models"][model_type])
-            graph_trainer = Graph_Trainer(model, **model_config["models"][model_type])
-            graph_trainer.fit(dataset, true_train_idx, true_tuning_idx, n_epochs=args.n_epochs)
+            model = get_model(model_type, num_features, num_classes, **model_config["models_config"][model_type])
+            graph_trainer = Graph_Trainer(model, **training_config["training_protocols"][model_type])
+            graph_trainer.fit(dataset, true_train_idx, true_tuning_idx, args.save_model, **training_config["training_protocols"][model_type])
             graph_trainer.test(dataset, test_idx)
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--dataset", help="Specify which dataset to use", type=parse_dataset, default="Cora")
     parser.add_argument("--model", help="Specify which model(s) you want to train and use", type=parse_model, default="GCN")
     parser.add_argument("--random_seed", help="Specify the seed", type=int, default=7)
-    parser.add_argument("--model_config_file", help="name of the model config file", default="models_config_file.yaml", type=str)
+    parser.add_argument("--models_config_file", help="name of the model config file", default="models_config_file.yaml", type=str)
+    parser.add_argument("--training_config_file", help="name of the training config_file", default="training_config_file.yaml", type=str)
     parser.add_argument("--save_model", help="Boolean flag indicating if model should be saved", action="store_true")
+
+    #We add the homophile argument. However, this is only used for the MixHop dataset
     parser.add_argument("--homophily", help="Level of homophily. This is only used for the 'Mixhop' dataset."
                                             "Needs to be in range [0.0 - 0.9]", default=0.6, type=float)
-    parser.add_argument("--n_epochs", help="Number of epochs to train the algorithm",
-                        default=200, type=int)
 
     args = parser.parse_args()
-    model_config = load_yaml_config_file(os.path.join(CONFIG_PATH, args.model_config_file))
+
     #Start training
     main(args)
 
-
 #ToDo: 25.01.2024
+# Synthetic Cora -> Gradient-Gating for DEEP-MULTI RATE Learning on Graphs
 # Write conformal prediction split procedure
-# Train and save checkpoints for models
+# Train and save checkpoints for models -> early stoppin on validation loss!
 # Write the diffusion score procedure
 # Write performance metric scores; singlehit and efficiency
 # Look into setup.py/toml installation script
+
+# Start the presentation!
+#
 
 
 
